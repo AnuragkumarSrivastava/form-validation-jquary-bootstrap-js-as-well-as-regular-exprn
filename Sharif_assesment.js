ffb4cd97b1cb01b6d8124f235001a744894d3389
AnuragkumarSrivastava <39129862+AnuragkumarSrivastava@users.noreply.github.com>
2018-05-22 15:47:56 +0530

    sharif form validation

---
+++ Sharif_assesment.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+	$("#send").click(function(){
+		var uid=$("#Uid").val();
+		var pwd=$("#Pwd").val();
+		
+		alert(" The User Id is:"+uid);
+		alert(" The Password you Entered is:"+pwd);
+	});
+	
+
+});
